4ba0ef406044355fe3a0232e1526b682af82f539
Dhanenero27 <beatjamsquad27@gmail.com>
2024-03-14 15:42:49 +0800

    Bingo Files

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 console.log("Hello World.");
 
-function generateRandomNumber() {
-    let randomNum  = Math.floor(Math.random() * 16) + 1;
+generateRandomNumber(30);
 
-    if(randomNum >= 5) {
-        console.log (`Random Number is > = 5`);
-    } else {
-        console.log (`Random Number is < = 5`);
+function generateRandomNumber(max) {
+    let randomNum  = Math.floor(Math.random() * max ) + 1;
+
+    if(randomNum <= 15) {
+        return console.log (`Random number ${randomNum} is in B. 1 - 15`);
+
+    } else if  (randomNum >= 16 && randomNum <= 30) {
+        return console.log(`Random number ${randomNum} is in I. 16 - 30`);
+
+    } else if  (randomNum >= 16 && randomNum <= 45) {
+        return console.log(`Random number ${randomNum} is in N. 31 - 45`);
+
+    } else if  (randomNum >= 16 && randomNum <= 60) {
+        return console.log(`Random number ${randomNum} is in G. 46 - 60`);
+
+    } else if  (randomNum >= 16 && randomNum <= 75) {
+        return console.log(`Random number ${randomNum} is in O. 61 - 75`);
+        
+        return console.log (`Random number is invalid`);
     }
 
-    return console.log(randomNum);
+ 
 }
